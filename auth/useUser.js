13a16650d31dd16074dcc073ba8d75a827b6acf0
Michel Dore <michel@doremail.nl>
2022-05-08 08:38:27 +0200

    Updated code to work with modular firebase SDK

--- auth/useUser.js
+++ auth/useUser.js
@@ -1,67 +1,67 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import "firebase/compat/auth";
+import { onIdTokenChanged, getAuth, signOut } from "@firebase/auth";
 
-import initFirebase from '../config';
+import initFirebase from "../config";
 import {
-  removeUserCookie,
-  setUserCookie,
-  getUserFromCookie
-} from './userCookie';
+    removeUserCookie,
+    setUserCookie,
+    getUserFromCookie,
+} from "./userCookie";
 
-initFirebase();
+const firebaseApp = initFirebase();
 
-export const mapUserData = async user => {
-  const { uid, email } = user;
-  const token = await user.getIdToken(true);
-  return {
-    id: uid,
-    email,
-    token
-  };
+export const mapUserData = async (user) => {
+    const { uid, email } = user;
+    const token = await user.getIdToken(true);
+    return {
+        id: uid,
+        email,
+        token,
+    };
 };
 
-  
 const useUser = () => {
-  const [user, setUser] = useState();
-  const router = useRouter();
+    const [user, setUser] = useState();
+    const router = useRouter();
 
-  const logout = async () => {
-    return firebase
-      .auth()
-      .signOut()
-      .then(() => {
-        router.push('/');
-      })
-      .catch(e => {
-        console.error(e);
-      });
-  };
+    const logout = async () => {
+        const auth = getAuth(firebaseApp);
+        return signOut(auth)
+            .then(() => {
+                router.push("/");
+            })
+            .catch((e) => {
+                console.error(e);
+            });
+    };
 
-  useEffect(() => {
-    const cancelAuthListener = firebase
-      .auth()
-      .onIdTokenChanged(async userToken => {
-        if (userToken) {
-          const userData = await mapUserData(userToken);
-          setUserCookie(userData);
-          setUser(userData);
-        } else {
-          removeUserCookie();
-          setUser();
+    useEffect(() => {
+        const auth = getAuth(firebaseApp);
+        const cancelAuthListener = onIdTokenChanged(
+            auth,
+            async function (userToken) {
+                if (userToken) {
+                    const userData = await mapUserData(userToken);
+                    setUserCookie(userData);
+                    setUser(userData);
+                } else {
+                    removeUserCookie();
+                    setUser();
+                }
+            }
+        );
+
+        const userFromCookie = getUserFromCookie();
+        if (!userFromCookie) {
+            return;
         }
-      });
+        setUser(userFromCookie);
+        return () => cancelAuthListener;
+    }, []);
 
-    const userFromCookie = getUserFromCookie();
-    if (!userFromCookie) {
-      return;
-    }
-    setUser(userFromCookie);
-    return () => cancelAuthListener;
-  }, []);
-
-  return { user, logout };
+    return { user, logout };
 };
 
 export { useUser };
--- pages/signin.js
+++ pages/signin.js
@@ -1,41 +1,43 @@
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import initFirebase from '../config';
-import { setUserCookie } from '../auth/userCookie';
-import { mapUserData } from '../auth/useUser';
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import "firebase/compat/auth";
+import initFirebase from "../config";
+import firebase from "firebase/compat/app";
+import { getAuth } from "@firebase/auth";
+import { setUserCookie } from "../auth/userCookie";
+import { mapUserData } from "../auth/useUser";
 
 initFirebase();
+
 const firebaseAuthConfig = ({ signInSuccessUrl }) => ({
-  signInFlow: 'popup',
-  signInOptions: [
-    {
-      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      requireDisplayName: false
+    signInFlow: "popup",
+    signInOptions: [
+        {
+            provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            requireDisplayName: false,
+        },
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    ],
+    signInSuccessUrl,
+    credentialHelper: "none",
+    callbacks: {
+        signInSuccessWithAuthResult: async ({ user }, redirectUrl) => {
+            const userData = await mapUserData(user);
+            setUserCookie(userData);
+        },
     },
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-  ],
-  signInSuccessUrl,
-  credentialHelper: 'none',
-  callbacks: {
-    signInSuccessWithAuthResult: async ({ user }, redirectUrl) => {
-      const userData = await mapUserData(user);
-      setUserCookie(userData);
-    }
-  }
 });
 
 const FirebaseAuth = () => {
-  const signInSuccessUrl = "/private"
-  return (
-    <div>
-      <StyledFirebaseAuth
-        uiConfig={firebaseAuthConfig({ signInSuccessUrl })}
-        firebaseAuth={firebase.auth()}
-        signInSuccessUrl={signInSuccessUrl}
-      />
-    </div>
-  );
+    const signInSuccessUrl = "/private";
+    return (
+        <div>
+            <StyledFirebaseAuth
+                uiConfig={firebaseAuthConfig({ signInSuccessUrl })}
+                firebaseAuth={getAuth()}
+                signInSuccessUrl={signInSuccessUrl}
+            />
+        </div>
+    );
 };
 
 export default FirebaseAuth;
